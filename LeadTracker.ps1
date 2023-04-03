c8f2d9d18d97b130f1fdf366bf41e5ffc27193c0
JStenoien <60165166+Jstenoien@users.noreply.github.com>
2023-04-03 15:26:52 -0700

    Update LeadTracker.ps1

--- LeadTracker.ps1
+++ LeadTracker.ps1
@@ -5,9 +5,9 @@ Add-Type -AssemblyName PresentationFramework
 
 #WPF XAML
 [XML]$form = @"
-<Window xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation" Topmost="True" WindowStyle="None" ResizeMode="NoResize" Background="Black" Title="Lead Tracker" Height="75" Width="155" AllowsTransparency="True">
+<Window xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation" Topmost="True" WindowStyle="None" ResizeMode="NoResize" Background="Black" Title="Lead Tracker" Height="70" Width="155" AllowsTransparency="True">
 	<Window.Clip>
-		<RectangleGeometry Rect="0,0,155,90" RadiusX="8" RadiusY="8"/>
+		<RectangleGeometry Rect="0,0,155,75" RadiusX="8" RadiusY="8"/>
 	</Window.Clip>
 	<Border BorderBrush="White" BorderThickness="1" CornerRadius="8">
 		<Grid>
@@ -19,7 +19,7 @@ Add-Type -AssemblyName PresentationFramework
 			<Grid.RowDefinitions>
 				<RowDefinition/>
 				<RowDefinition/>
-				<RowDefinition Height="1.75*"/>
+				<RowDefinition Height="1.5*"/>
 			</Grid.RowDefinitions>
 			<Label Foreground="White" HorizontalAlignment="Right" VerticalAlignment="Center" Margin="0" Padding="0,0,5,0" FontWeight="Bold" Grid.Row="0" Grid.Column="0" Content="Shift:"/>
 			<TextBox Name="TimeBox" VerticalContentAlignment="Center" HorizontalContentAlignment="Center" Grid.Row="0" Grid.Column="1"/>
@@ -33,7 +33,7 @@ Add-Type -AssemblyName PresentationFramework
 			</Button>
 			<Label Foreground="White" HorizontalAlignment="Right" VerticalAlignment="Center" Margin="0" Padding="0,0,5,0" FontWeight="Bold" Grid.Row="1" Grid.Column="0" Content="Leads:"/>
 			<TextBox Name="LeadBox" VerticalContentAlignment="Center" Padding="5,0,0,0" Grid.Row="1" Grid.Column="1"/>
-			<Label Foreground="White" FontSize="20" HorizontalAlignment="Center" VerticalAlignment="Center" FontWeight="Bold" Name="StatLabel" Grid.Row="2" Grid.Column="0" Grid.ColumnSpan="3" Content="">
+			<Label Foreground="White" FontSize="20" HorizontalAlignment="Center" Padding="0" VerticalAlignment="Center" FontWeight="Bold" Name="StatLabel" Grid.Row="2" Grid.Column="0" Grid.ColumnSpan="3" Content="">
 				<Label.ToolTip>
 					<ToolTip Name="StatsTT"/>
 				</Label.ToolTip>
